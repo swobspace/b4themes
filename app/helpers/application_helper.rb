2a2028d31a06fa71b98bfa206b7ac494301009d6
Wolfgang Barth <wob@swobspace.net>
2018-07-12 12:01:57 +0200

    initial checkin

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,26 @@
+module ApplicationHelper
+  THEME_COLORS=[ 'primary', 'secondary', 'success', 
+                 'danger', 'warning', 'info', 'light', 'dark' ]
+  def loremipsum(length)
+    lore =-<<EOIPSUM
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+EOIPSUM
+    lore[0..length - 1]
+  end
+
+  def all_alerts
+    THEME_COLORS.map { |color| alert(color) }.join("\n")
+  end
+                
+  def alert(color)
+    "<div class=\"alert alert-#{color}\" role=\"alert\">/#{color}/ #{loremipsum(200)}</div>"
+  end
+
+  def all_buttons
+    THEME_COLORS.map { |color| button(color) }.join("\n")
+  end
+
+  def button(color)
+    "<button type='button' class=\"btn btn-#{color} col-sm-3\">#{color}</button>"
+  end
+end
